68af6a4844dfe1ae27a7f139235fa0ef45ef54dd
Jems Crister22 <Cristerjeymss22@gmail.com>
2025-08-11 00:10:42 +0800

    Initial commit - CodeIgniter setup

---
+++ app/Controllers/Home.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controllers;
+
+class Home extends BaseController
+{
+    public function index(): string
+    {
+        $data['content'] = view('home_view');
+        return view('template', $data);
+    }
+}
